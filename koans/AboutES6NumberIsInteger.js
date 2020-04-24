3178d7da4ece4df62fab2a0e25031c236d2cd4e3
David Hernandez <davidhernandezruiz@gmail.com>
2020-04-24 13:37:04 +0200

    Adapts Number.IsInteger and Object.Is koans.

--- koans/AboutES6NumberIsInteger.js
+++ koans/AboutES6NumberIsInteger.js
@@ -3,68 +3,67 @@
 
 describe('`Number.isInteger()` determines if a value is an integer', () =>{
   it('`isInteger` is a static function on `Number`', () => {
-    const whatType = 'method';
-    expect(typeof Number.isInteger).toEqual(whatType)
+    expect(typeof Number.isInteger).toEqual(FILL_ME_IN)
   });
 
   describe('zero in different ways', () => {
     it('0 is an integer', () => {
-      const zero = null;
-      expect(Number.isInteger(zero)).toEqual(true)
+      const zero = 0;
+      expect(Number.isInteger(zero)).toEqual(FILL_ME_IN)
     });
 
     it('0.000', () => {
-      const veryZero = 0.000001;
-      expect(Number.isInteger(veryZero)).toEqual(true)
+      const veryZero = 0.00000;
+      expect(Number.isInteger(veryZero)).toEqual(FILL_ME_IN)
     });
 
     it('the string "0" is NOT an integer', () => {
-      const stringZero = 0;
-      expect(Number.isInteger(stringZero)).toEqual(false)
+      const stringZero = '0';
+      expect(Number.isInteger(stringZero)).toEqual(FILL_ME_IN)
     });
   });
 
   describe('one in different ways', () => {
     it('0.111 + 0.889', () => {
-      const rest = 0.88;
-      expect(Number.isInteger(0.111 + rest)).toEqual(true)
+      const rest = 0.889;
+      expect(Number.isInteger(0.111 + rest)).toEqual(FILL_ME_IN)
     });
 
     it('0.5 + 0.2 + 0.2 + 0.1 = 1 ... isn`t it?', () => {
-      const oneOrNot = 0.5 + 0.2 + 0.3;
-      expect(Number.isInteger(oneOrNot)).toEqual(false)
+      const oneOrNot = 0.5 + 0.2 + 0.2 + 0.1;
+      expect(Number.isInteger(oneOrNot)).toEqual(FILL_ME_IN)
     });
 
     it('parseInt`ed "1" is an integer', () => {
-      const convertedToInt = Number.parse('1.01');
-      expect(Number.isInteger(convertedToInt)).toEqual(true)
+      const convertedToInt = Number.parseInt('1.01');
+      expect(Number.isInteger(convertedToInt)).toEqual(FILL_ME_IN)
     });
   });
 
-  describe('what is not an integer', () => {
+  xdescribe('what is not an integer', () => {
     it('`Number()` is an integer', () => {
-      const numberOne = Number;
-      expect(Number.isInteger(numberOne)).toEqual(true)
+      const numberOne = Number();
+      expect(Number.isInteger(numberOne)).toEqual(FILL_ME_IN)
     });
 
     it('`{}` is NOT an integer', () => {
-      const isit = Number.isWhat({});
-      expect(isit).toEqual(false)
+      const isit = Number.isInteger({});
+      expect(isit).toEqual(FILL_ME_IN)
     });
 
     it('`0.1` is not an integer', () => {
-      const isit = Number(0.1);
-      expect(isit).toEqual(false)
+      const isit = Number.isInteger(0.1);
+      expect(isit).toEqual(FILL_ME_IN)
     });
 
     it('`Number.Infinity` is not an integer', () => {
-      const isit = Number.isInteger(Number.MAX_VALUE);
-      expect(isit).toEqual(false)
+      const isit = Number.isInteger(Number.Infinity);
+      expect(isit).toEqual(FILL_ME_IN)
     });
 
     it('`NaN` is not an integer', () => {
-      const isit = Number.isFloat(NaN);
-      expect(isit).toEqual(false)
+      const isit = Number.isInteger(NaN);
+      expect(isit).toEqual(FILL_ME_IN)
     });
   });
 });
--- koans/AboutES6ObjectIs.js
+++ koans/AboutES6ObjectIs.js
@@ -4,68 +4,57 @@
 describe('`Object.is()` determines whether two values are the same', () =>{
   describe('scalar values', () => {
     it('1 is the same as 1', () => {
-      const areSame = Object.is(1, '???');
-      expect(areSame).toEqual(false);
+      const areSame = Object.is(1, 1);
+      expect(areSame).toEqual(FILL_ME_IN);
     });
 
     it('int 1 is different to string "1"', () => {
-      const areSame = Object.___(1, '1');
-      expect(areSame).toEqual(false);
+      const areSame = Object.is(1, '1');
+      expect(areSame).toEqual(FILL_ME_IN);
     });
 
     it('strings just have to match', () => {
-      const areSame = Object.is('one', 'two');
-      expect(areSame).toEqual(false);
+      const areSame = Object.is('one', 'one');
+
+      expect(areSame).toEqual(FILL_ME_IN);
     });
 
     it('+0 is not the same as -0', () => {
-      const areSame = -1;
-      expect(Object.is(+0, -0)).toEqual(areSame);
+      expect(Object.is(+0, -0)).toEqual(FILL_ME_IN);
     });
 
     it('NaN is the same as NaN', () => {
-      const number = 0;
-      expect(Object.is(NaN, number)).toEqual(true);
+      expect(Object.is(NaN, FILL_ME_IN)).toEqual(true);
     });
   });
 
   describe('coercion, as in `==` and `===`, does NOT apply', () => {
     it('+0 != -0', () => {
       const coerced = +0 === -0;
-      const isSame = Object.is(+0, -0);
-      expect(isSame).toEqual(coerced);
+
+      expect(coerced).toEqual(FILL_ME_IN);
     });
 
     it('empty string and `false` are not the same', () => {
       const emptyString = '';
       const isSame = Object.is(emptyString, false);
-      expect(isSame, emptyString).toEqual(false);
+      expect(isSame).toEqual(FILL_ME_IN);
     });
 
     it('NaN', () => {
       const coerced = NaN == NaN;
-      const isSame = Object.is(NaN, NaN);
-      expect(isSame).toEqual(coerced);
+      expect(coerced).toEqual(FILL_ME_IN);
     });
 
     it('NaN 0/0', () => {
-      const isSame = Object.ISSSSS(NaN, 0/0);
-      expect(isSame).toEqual(true);
+      const isSame = Object.is(NaN, 0/0);
+      expect(isSame).toEqual(FILL_ME_IN);
     });
   });
 
   describe('complex values', () => {
     it('`{}` is just not the same as `{}`', () => {
-      const areSame = '???';
-      expect(isSame).toEqual(true);
-      expect(Object.is({}, {})).toEqual(areSame);
-    });
-
-    it('Map', () => {
-      let map1 = new Map([[1, 'one']]);
-      let map2 = new Map([[1, 'one']]);
-      const areSame = Object.is(map1, map2);
-      expect(areSame).toEqual(false);
+      expect(Object.is({}, {})).toEqual(FILL_ME_IN);
     });
   });
 });
